5bafc08f8a7560e0ca65e02241807702a99143d2
arumlee <arumlee.above@gmail.com>
2023-01-25 04:02:15 +0900

    add withCredentials

--- src/lib/customAxios.ts
+++ src/lib/customAxios.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
 
+axios.defaults.withCredentials = true;
+
 export const customAxios: AxiosInstance = axios.create({
   baseURL: "http://54.238.154.254:8080", // 기본 서버 주소 입력
 });
